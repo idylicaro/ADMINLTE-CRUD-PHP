3824def7de13434dabc5bab8caaf680bf1341809
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-01 17:34:57 -0300

    chore: init configs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cb4235e351776a8cc93eec05ca143321a99eda0b',
+        'name' => 'idylicaro/admin-lte-php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'idylicaro/admin-lte-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cb4235e351776a8cc93eec05ca143321a99eda0b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
